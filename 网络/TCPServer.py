9b31cb5fbea567293745b18090a2a72413fdb937
花不休 <1347638343@qq.com>
2018-04-21 14:41:05 +0800

    tcp

--- 网络/TCPServer.py
+++ 网络/TCPServer.py
@@ -3,14 +3,13 @@ serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
 serverSocket.listen(1)
-print('The server is ready to receive')
 
-while 1:
-	connectionSocket, addr = serverSocket.accept()
-	#connectionSocket.send('123456')
-	sentence = connectionSocket.recv(1024)
-	capitalizedSentence = sentence.upper()
-	print(capitalizedSentence)
-	connectionSocket.send(capitalizedSentence)
-	connectionSocket.close()
+print('The server is ready to receive')
+connectionSocket, addr = serverSocket.accept()
+connectionSocket.send('123456')
+sentence = connectionSocket.recv(1024)
+capitalizedSentence = sentence.upper()
+print(capitalizedSentence)
+connectionSocket.send(capitalizedSentence)
+connectionSocket.close()
 
