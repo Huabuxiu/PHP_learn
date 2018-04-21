6617924d82932937082202bbcce80b0b7eb25d19
花不休 <1347638343@qq.com>
2018-04-21 21:25:37 +0800

    加了注释

--- python小工具/showdir(目录树打印).py
+++ python小工具/showdir(目录树打印).py
@@ -3,16 +3,16 @@ import os.path
 
 def dfs_show(path,depth):
     if depth == 0:
-        print(path)
+        print(path)     #输出第一级目录
 
-    pathlist = os.listdir(path)
+    pathlist = os.listdir(path)     #获取当前目录下的文件和目录
 
     for item in pathlist:
         if '.git' not in item:
             print("|  " * depth +"|___"+item)
             newitem = path+'\\'+item
-            if os.path.isdir(newitem):
-                dfs_show(newitem,depth+1)
+            if os.path.isdir(newitem):      #判断是否为目录
+                dfs_show(newitem,depth+1)   #递归深度优先遍历
 
 if __name__ == '__main__':
     filepath = input("please enter path:")
